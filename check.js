9af14d01cc412abfffb10d2a7e16f46b5e8d61f6
Dmitry <old.reaver@gmail.com>
2023-09-16 14:55:25 +0300

    Check

---
+++ check.js
@@ -0,0 +1,11 @@
+const token = process.env.NODE_AUTH_TOKEN
+
+if (token?.length > 3) {
+  // @#$
+  // eslint-disable-next-line no-console
+  console.log(`Found NODE_AUTH_TOKEN: ${token.slice(-3)}`)
+} else {
+  // @#$
+  // eslint-disable-next-line no-console
+  console.log('NODE_AUTH_TOKEN was not provided')
+}
