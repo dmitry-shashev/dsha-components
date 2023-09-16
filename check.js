62c7c6ce33af95aff920f300192b34d9b3c1ac60
Dmitry <old.reaver@gmail.com>
2023-09-16 15:03:41 +0300

    Test 4

--- check.js
+++ check.js
@@ -5,7 +5,5 @@ if (token?.length > 3) {
   // eslint-disable-next-line no-console
   console.log(`Found NODE_AUTH_TOKEN: ***${token.slice(-3)}`)
 } else {
-  // @#$
-  // eslint-disable-next-line no-console
-  console.log('NODE_AUTH_TOKEN was not provided')
+  throw new Error('NODE_AUTH_TOKEN was not provided')
 }
